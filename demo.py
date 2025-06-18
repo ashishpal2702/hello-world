978780defb55fc9d993e79ab9a59da745b56c755
Ashish Pal <ashishpal2702@gmail.com>
2025-06-18 15:57:49 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,7 +1,23 @@
 print("Hello World")
-def area_of_cirle(r):
-  pi = 3.14
-  return pi*r*r
-## Print Area of Circle
-area = area_of_cirle(10)
-print(area)
+def Area_of_cirle(r):
+  """
+  Area of Circle
+  
+  Args: 
+  r (float) : Area of Circle
+
+  Returns:
+  float : Area of Circle
+  """
+    pi = 3.14
+    return pi*r*r
+
+def main():
+  ## Print Area of Circle
+  area = area_of_cirle(10)
+  print(area)
+
+
+if __name__ == "__main__":
+  main()
+  
